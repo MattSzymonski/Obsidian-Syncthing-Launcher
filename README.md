d9848f0c7529d3f9376680f5ba33d21bd4870e3e
mattszymonski <mateusz.szymonski@gmail.com>
2024-03-04 02:21:14 +0100

    Getting latest syncthing action

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Obsidian plugin designed to run Syncthing synchronization software directly as a subprocess of Obsidian, providing a convenient solution for users who prefer not to install Syncthing as a standalone application. 
 
-🚀 Features
+## 🚀 Features
 - Embedded Syncthing executable: No separate installation required! The Syncthing integrated within the plugin.
 - Syncthing running status: Status indicator located in the bottom-right status bar of Obsidian. The status is represented as:
   - 🔵 Syncthing Running: Indicates that Syncthing is actively running and your files are being synced.
@@ -10,8 +10,8 @@ Obsidian plugin designed to run Syncthing synchronization software directly as a
 - Last Sync Timestamp: Time of the last successful synchronization directly within the status bar.
 - Automatic Syncthing start and close: Syncthing is started on Obsidian open and closed on its close.
 
-🛠️ Getting Started
+## 🛠️ Getting Started  
 WIP
 
-💡 Other
+## 💡 Other
 - It can work as a supplement to [Obsidian Syncthing Integration](https://github.com/LBF38/obsidian-syncthing-integration) plugin.
--- main.ts
+++ main.ts
@@ -1,26 +1,35 @@
 import { App, Editor, FileSystemAdapter, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, addIcon } from 'obsidian';
 import { ChildProcessWithoutNullStreams, exec, spawn } from "child_process";
+import axios from 'axios';
 
 export default class MyPlugin extends Plugin {
 	statusBarItem: HTMLElement | null = this.addStatusBarItem();
 
 	syncthingInstance: ChildProcessWithoutNullStreams | null = null;
-	isSyncthingConnected: boolean = false;
 	syncthingLastSyncDate: Date = new Date();
 
+	syncFolderID: string = 'ms-ov';
+	syncthingApiKey: string = 'isoDgfErkUr7GSzPTPsMiaHNsJqLvMuS';
+	syncthingUrl = 'http://127.0.0.1:8384/';
+
+	
 	updateStatusBar(): void {
-		if (this.statusBarItem) {
-			const connectionStatus = this.isSyncthingConnected ? "🔵" : "⚫";
+		this.isSyncthingRunning().then(isRunning => {
+			
+			this.syncthingLastSyncDate = new Date();
+
+			const connectionStatus = isRunning ? "🔵" : "⚫";
 			const lastSyncTime = this.syncthingLastSyncDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
 
-			this.statusBarItem.setText(`${connectionStatus} Last sync: ${lastSyncTime}`);
-		}
+			this.statusBarItem?.setText(`${connectionStatus} Last sync: ${lastSyncTime}`);
+		});
 	}
 
 	async onload() {
 		this.statusBarItem?.onClickEvent((event) => {
-			new Notice('Starting Syncthing!');
+			new Notice('Starting Syncthingx!');
 			this.startSyncthing();
+			this.getLastSyncDate();
 		});
 
 		// Add css class to status bar item
@@ -50,39 +59,81 @@ export default class MyPlugin extends Plugin {
 	}
 
 	startSyncthing() {
-		const executablePath = this.getAbsolutePath("syncthing.exe");
-		this.syncthingInstance = spawn(executablePath, []);
+		this.isSyncthingRunning().then(isRunning => {
+			// Check if already running
+			if (isRunning) {
+				console.log('Syncthing is already running');
+				return;
+			}
 
-		this.isSyncthingConnected = this.syncthingInstance.pid !== undefined;
-
-		this.syncthingInstance.stdout.on('data', (data) => {
-			console.log(`stdout: ${data}`);
-		});
-
-		this.syncthingInstance.stderr.on('data', (data) => {
-			console.error(`stderr: ${data}`);
-			this.isSyncthingConnected = false;
-		});
-
-		this.syncthingInstance.on('exit', (code) => {
-			console.log(`child process exited with code ${code}`);
-			this.isSyncthingConnected = false;
+			// Start
+			const executablePath = this.getPluginAbsolutePath() + "syncthing/syncthing.exe";
+			this.syncthingInstance = spawn(executablePath, []);
+	
+			this.syncthingInstance.stdout.on('data', (data) => {
+				console.log(`stdout: ${data}`);
+			});
+	
+			this.syncthingInstance.stderr.on('data', (data) => {
+				console.error(`stderr: ${data}`);
+			});
+	
+			this.syncthingInstance.on('exit', (code) => {
+				console.log(`child process exited with code ${code}`);
+			});
 		});
 	}
 
-	getAbsolutePath(fileName: string): string {
+	async isSyncthingRunning(): Promise<boolean> {
+		const config = {
+			headers: {
+				'X-API-Key': this.syncthingApiKey,
+			}
+		};
+		
+		return axios.get(this.syncthingUrl, config)
+			.then(response => true)
+			.catch(error => { console.log("Syncthing not running"); return false; });
+	}
+
+	getPluginAbsolutePath(): string {
         let basePath;
         let relativePath;
+
         // base path
         if (this.app.vault.adapter instanceof FileSystemAdapter) {
             basePath = this.app.vault.adapter.getBasePath();
         } else {
             throw new Error('Cannot determine base path.');
         }
+
         // relative path
-        relativePath = `${this.app.vault.configDir}/plugins/obsidian-sample-plugin/syncthing/${fileName}`;
+        relativePath = `${this.app.vault.configDir}/plugins/${this.manifest.id}-${this.manifest.version}/`;
+
         // absolute path
         return `${basePath}/${relativePath}`;
     }
+
+	async getLastSyncDate() {
+		try {
+		  const response = await axios.get(this.syncthingUrl, {
+			headers: {
+			  'X-API-Key': this.syncthingApiKey
+			}
+		  });
+	  
+		  if (response.data && response.data.lastScan) {
+			const lastSyncDate = new Date(response.data.lastScan);
+			console.log(`Last sync date for folder ${this.syncFolderID}:`, lastSyncDate);
+			return lastSyncDate;
+		  } else {
+			console.log('No sync data found.');
+			return null;
+		  }
+		} catch (error) {
+		  console.error('Failed to get last sync date:', error);
+		  return null;
+		}
+	}
 }
 

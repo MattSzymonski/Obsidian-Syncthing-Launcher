d4bbc6439fdd13d91b3e42ffba901b450147a018
mattszymonski <mateusz.szymonski@gmail.com>
2024-03-05 01:05:14 +0100

    Status bar style

--- main.ts
+++ main.ts
@@ -2,62 +2,55 @@ import { App, Editor, FileSystemAdapter, MarkdownView, Modal, Notice, Plugin, Pl
 import { ChildProcessWithoutNullStreams, exec, spawn } from "child_process";
 import axios from 'axios';
 
-export default class MyPlugin extends Plugin {
-	statusBarItem: HTMLElement | null = this.addStatusBarItem();
+interface Settings {
+	syncthingApiKey: string;
+	vaultFolderID: string;
+}
 
-	syncthingInstance: ChildProcessWithoutNullStreams | null = null;
-	syncthingLastSyncDate: string = "no data";
+const DEFAULT_SETTINGS: Settings = {
+	syncthingApiKey: '',
+	vaultFolderID: ''
+}
 
-	syncFolderID: string = 'ms-ov';
-	syncthingApiKey: string = 'isoDgfErkUr7GSzPTPsMiaHNsJqLvMuS';
-	syncthingUrl = 'http://127.0.0.1:8384/';
-	updateInterval: number = 5000;
+export default class SyncthingLauncher extends Plugin {
+	public settings: Settings;
 
-	updateStatusBar(): void {
-		this.isSyncthingRunning().then(isRunning => {
-			
-			if (isRunning) {
-				this.getLastSyncDate().then(lastSyncDate => {
-					if (lastSyncDate !== null)
-					{
-						const optionsDate: Intl.DateTimeFormatOptions = { day: '2-digit', month: '2-digit', year: '2-digit' };
-						const formattedDate = lastSyncDate.toLocaleDateString('en-GB', optionsDate).split( '/' ).reverse( ).join( '.' );
+	private syncthingUrl = 'http://127.0.0.1:8384/';
+	private updateInterval: number = 1000;
 
-						const optionsTime: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit', hour12: false };
-						const formattedTime = lastSyncDate.toLocaleTimeString('en-GB', optionsTime); 
+	private syncthingInstance: ChildProcessWithoutNullStreams | null = null;
+	private syncthingLastSyncDate: string = "no data";
 
-						this.syncthingLastSyncDate = `${formattedDate} ${formattedTime}`;
-					}
-					else
-					{
-						this.syncthingLastSyncDate = "no data";
-					}
-				});
-			}
-			
-			const connectionStatus = isRunning ? "🔵" : "⚫";
-			this.statusBarItem?.setText(`${connectionStatus} Last sync: ${this.syncthingLastSyncDate}`);
-		});
-	}
+	private statusBarConnectionIconItem: HTMLElement | null = this.addStatusBarItem();
+	private statusBarLastSyncTextItem: HTMLElement | null = this.addStatusBarItem();
 
 	async onload() {
-		this.statusBarItem?.onClickEvent((event) => {
-			new Notice('Starting Syncthing!');
-			this.startSyncthing();
-		});
+		await this.loadSettings();
 
-		// Add css class to status bar item
-		this.statusBarItem?.addClass('status-bar-item');
+		this.statusBarConnectionIconItem?.addClasses(['status-bar-item', 'status-icon']);
+		this.statusBarConnectionIconItem?.setAttribute('data-tooltip-position', 'top');
+		this.statusBarLastSyncTextItem?.addClass('status-bar-item');
+
+		this.statusBarConnectionIconItem?.onClickEvent((event) => {
+			this.isSyncthingRunning().then(isRunning => {
+				if (!isRunning) {
+					new Notice('Starting Syncthing!');
+					this.startSyncthing();
+				}
+			}
+		)});
 
 		// Update syncthing the status bar item
 		this.updateStatusBar();
 
+		// Register tick interval
 		this.registerInterval(
 			window.setInterval(() => this.updateStatusBar(), this.updateInterval)
 		);
+
+		// Resister settings tab
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
-
-
 
 	onunload() {
 		// Kill syncthing if running
@@ -73,6 +66,18 @@ export default class MyPlugin extends Plugin {
 			});
 		}
 	}
+
+	// --- Settings ---
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	// --- Logic ---
 
 	startSyncthing() {
 		this.isSyncthingRunning().then(isRunning => {
@@ -100,10 +105,48 @@ export default class MyPlugin extends Plugin {
 		});
 	}
 
+	updateStatusBar(): void {
+		this.isSyncthingRunning().then(isRunning => {
+			
+			if (isRunning) {
+				this.getLastSyncDate().then(lastSyncDate => {
+					if (lastSyncDate !== null)
+					{
+						const optionsDate: Intl.DateTimeFormatOptions = { day: '2-digit', month: '2-digit', year: '2-digit' };
+						const formattedDate = lastSyncDate.toLocaleDateString('en-GB', optionsDate).split( '/' ).join( '.' );
+
+						const optionsTime: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit', hour12: false };
+						const formattedTime = lastSyncDate.toLocaleTimeString('en-GB', optionsTime); 
+
+						this.syncthingLastSyncDate = `${formattedDate} ${formattedTime}`;
+					}
+					else {
+						this.syncthingLastSyncDate = "no data";
+					}
+				});
+			}
+			
+			if (this.statusBarConnectionIconItem) {
+				this.statusBarConnectionIconItem.setText(isRunning ? "🔵" : "⚫");
+				this.statusBarConnectionIconItem.ariaLabel = isRunning ? "Syncthing connected" : "Click to start Syncthing";
+				if (isRunning) {
+					this.statusBarConnectionIconItem.removeClasses(['plugin-editor-status', 'mouse-pointer']);
+				}
+				else {
+					this.statusBarConnectionIconItem.addClasses(['plugin-editor-status', 'mouse-pointer']);
+				}
+			}
+
+			if (this.statusBarLastSyncTextItem) {
+				this.statusBarLastSyncTextItem.setText(`Last sync: ${this.syncthingLastSyncDate}`);
+			}
+		});
+	}
+
 	async isSyncthingRunning(): Promise<boolean> {
 		const config = {
 			headers: {
-				'X-API-Key': this.syncthingApiKey,
+				'X-API-Key': this.settings.syncthingApiKey,
 			}
 		};
 		
@@ -131,9 +174,9 @@ export default class MyPlugin extends Plugin {
 
 	async getLastSyncDate() {
 		try {
-		  const response = await axios.get(this.syncthingUrl + `rest/db/status?folder=${this.syncFolderID}`, {
+		  const response = await axios.get(this.syncthingUrl + `rest/db/status?folder=${this.settings.vaultFolderID}`, {
 			headers: {
-			  'X-API-Key': this.syncthingApiKey
+			  'X-API-Key': this.settings.syncthingApiKey,
 			}
 		  });
 
@@ -147,6 +190,43 @@ export default class MyPlugin extends Plugin {
 		  console.error('Failed to get last sync date:', error);
 		  return null;
 		}
+	}
+}
+
+class SettingTab extends PluginSettingTab {
+	plugin: SyncthingLauncher;
+
+	constructor(app: App, plugin: SyncthingLauncher) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Syncthing API key')
+			.setDesc('API key of Syncthing instance (in Syncthing GUI -> Actions -> Settings)')
+			.addText(text => text
+				.setPlaceholder('Enter Syncthing API key')
+				.setValue(this.plugin.settings.syncthingApiKey)
+				.onChange(async (value) => {
+					this.plugin.settings.syncthingApiKey = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Vault folder ID')
+			.setDesc('ID of the folder in which the vault is stored (in Syncthing GUI -> Folders -> Vault folder)')
+			.addText(text => text
+				.setPlaceholder('Enter vault folder ID')
+				.setValue(this.plugin.settings.vaultFolderID)
+				.onChange(async (value) => {
+					this.plugin.settings.vaultFolderID = value;
+					await this.plugin.saveSettings();
+				}));
 	}
 }
 
